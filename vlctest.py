cf6d9ec02302105f091b4a929a8a5848af6c60da
syeyoung <cyoung06@naver.com>
2023-12-27 15:31:06 +0900

    test

---
+++ vlctest.py
@@ -0,0 +1,25 @@
+import vlc
+
+player = vlc.MediaPlayer()
+
+# media object 
+media = vlc.Media("videos/defend2.mp4") 
+  
+# setting media to the media player 
+media_player.set_media(media) 
+  
+  
+  
+# start playing video + commenting it  
+media_player.play() 
+  
+# wait so the video can be played for 5 seconds 
+# irrespective of length of video 
+time.sleep(5) 
+  
+# checking if it is playing 
+value = media_player.is_playing() 
+  
+# printing value 
+print("Is playing : ") 
+print(value) 
